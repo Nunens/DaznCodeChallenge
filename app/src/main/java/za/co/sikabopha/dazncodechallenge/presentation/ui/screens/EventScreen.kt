6010c182b0e1ead4687bc3d5ae7cbfc7b445ed31
S Kobue <siphokobue@gmail.com>
2022-07-28 07:31:04 +0200

    Move list above navBar

--- app/src/main/java/za/co/sikabopha/dazncodechallenge/presentation/ui/screens/EventScreen.kt
+++ app/src/main/java/za/co/sikabopha/dazncodechallenge/presentation/ui/screens/EventScreen.kt
@@ -1,9 +1,12 @@
 package za.co.sikabopha.dazncodechallenge.presentation.ui.screens
 
 import android.content.Context
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import za.co.sikabopha.dazncodechallenge.domain.model.Event
 import za.co.sikabopha.dazncodechallenge.presentation.ui.components.EventItem
 import za.co.sikabopha.dazncodechallenge.presentation.viewmodel.DaznViewModel
@@ -15,7 +18,7 @@ fun EventScreen(vm: DaznViewModel, context: Context) {
 
 @Composable
 fun EventList(eventList: List<Event>, context: Context) {
-    LazyColumn {
+    LazyColumn(modifier = Modifier.padding(bottom = 55.dp)) {
         itemsIndexed(items = eventList) { _, item ->
             EventItem(event = item, context = context)
         }
--- app/src/main/java/za/co/sikabopha/dazncodechallenge/presentation/ui/screens/ScheduleScreen.kt
+++ app/src/main/java/za/co/sikabopha/dazncodechallenge/presentation/ui/screens/ScheduleScreen.kt
@@ -1,9 +1,12 @@
 package za.co.sikabopha.dazncodechallenge.presentation.ui.screens
 
 import android.content.Context
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import za.co.sikabopha.dazncodechallenge.domain.model.Schedule
 import za.co.sikabopha.dazncodechallenge.presentation.ui.components.ScheduleItem
 import za.co.sikabopha.dazncodechallenge.presentation.viewmodel.DaznViewModel
@@ -15,7 +18,7 @@ fun ScheduleScreen(vm: DaznViewModel, context: Context) {
 
 @Composable
 fun ScheduleList(scheduleList: List<Schedule>, context: Context) {
-    LazyColumn {
+    LazyColumn(modifier = Modifier.padding(bottom = 55.dp)) {
         itemsIndexed(items = scheduleList) { _, item ->
             ScheduleItem(schedule = item, context = context)
         }
